56df8bd6dd61dc4c2894fa6c111dd77d29276a24
lucca713 <luccacerasomma03@gmail.com>
2026-09-17 23:49:31 -0300

    criando a qtable e a get values para a qtable

--- projeto/robo-labirinto/src/agent/agent.service.ts
+++ projeto/robo-labirinto/src/agent/agent.service.ts
@@ -4,5 +4,21 @@ import { Injectable } from "@nestjs/common";
 @Injectable()
 export class AgenteService {
     private Qtable: Map<number, number[]> = new Map()
-    
+
+    private CriandoAQtable(): number[] {
+        //somente cria a Qtable e comeca o primeiro estado como zero
+        this.Qtable.set(0, [0, 0, 0, 0])
+
+        return [0, 0, 0, 0]
+    }
+
+    private getQValues(estado: number): number[] {
+        const valores = this.Qtable.get(estado)
+
+        if (!valores) return this.CriandoAQtable()
+
+        return valores
+
+    }
 }
+
