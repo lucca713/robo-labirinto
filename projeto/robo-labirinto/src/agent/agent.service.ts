43f2a0a127dae40a1b506823d73386c4136adcf4
lucca713 <luccacerasomma03@gmail.com>
2026-09-21 08:28:40 -0300

    Feat: adicionando a funcao de preencher a Q-table

--- projeto/robo-labirinto/src/agent/agent.service.ts
+++ projeto/robo-labirinto/src/agent/agent.service.ts
@@ -11,6 +11,13 @@ export class AgenteService {
 
         return [0, 0, 0, 0]
     }
+    private PreencherQtable(estado: number, acao: number, recompensa: number): void {
+        const acoesDoEstado = this.Qtable.get(estado);
+
+        acoesDoEstado[acao] = recompensa;
+
+        this.Qtable.set(estado, acoesDoEstado)
+    }
 
     private getQValues(estado: number): number[] {
         const valores = this.Qtable.get(estado)
@@ -18,7 +25,9 @@ export class AgenteService {
         if (!valores) return this.CriandoAQtable()
 
         return valores
-
     }
+
+
+
 }
 
